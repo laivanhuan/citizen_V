c1f19e30f0bd114c968bf99809eb131184a81b17
Lai Huan <laivanhuan2104@gmail.com>
2021-12-16 20:31:39 +0700

    hot fix add api create village

--- routes/location.route.js
+++ routes/location.route.js
@@ -1,8 +1,11 @@
 const express = require("express");
+
 const { localtionController } = require('../controllers');
+const middlewares = require('../middlewares');
 
 const router = express.Router();
 
 router.get("/", localtionController.getLocation);
+router.post("/village", middlewares.authenticate, localtionController.createVillage);
 
 module.exports = router;
