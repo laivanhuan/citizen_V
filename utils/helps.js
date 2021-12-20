1009fc5d92b33e17f7c4261ea69981453250ff7b
Lai Huan <laivanhuan2104@gmail.com>
2021-12-20 21:40:17 +0700

    hot fix

--- utils/helps.js
+++ utils/helps.js
@@ -1,16 +1,16 @@
 const {districts, wards, villages} = require('../models');
 
 const checkAddress = async (province_id=null, district_id=null, ward_id=null, village_id=null) => {
-    let flag = false;
+    let flag = true;
     if(province_id && district_id && ward_id && village_id) {
         const address = await villages.findOne({where: {province_id, district_id, ward_id, id: village_id}});
-        if(address) flag = true;
+        if(!address) flag = false;
     } else if(province_id && district_id && ward_id) {
         const address = await wards.findOne({where: {province_id, district_id, id: ward_id}});
-        if(address) flag = true;
+        if(!address) flag = false;
     } else if(province_id && district_id) {
         const address = await districts.findOne({where: {province_id, id: district_id}});
-        if(address) flag = true;
+        if(!address) flag = false;
     }
     return flag;
 }
